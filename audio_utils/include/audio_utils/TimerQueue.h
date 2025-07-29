967f318adbc5de0230f3eb90de654837579b0655
Andy Hung <hunga@google.com>
2025-07-29 16:57:08 -0700

    TimerQueue: Add soft wakeup deadlines
    Flag: EXEMPT bugfix
    Test: atest timerqueue_tests
    Bug: 432316839
    Change-Id: I0644e88fdb6675de9ebd11201e0678722b64349d

--- audio_utils/include/audio_utils/TimerQueue.h
+++ audio_utils/include/audio_utils/TimerQueue.h
@@ -21,11 +21,84 @@
 #include <condition_variable>
 #include <functional>
 #include <map>
+#include <memory>
 #include <mutex>
+#include <set>
 #include <thread>
+#include <utils/Errors.h>
 #include <utils/Timers.h> // For nsecs_t
 
 namespace android::audio_utils {
+
+/**
+ * An interface for a clock that can create and wait for timers.
+ * This is used to abstract away the Linux-specific timerfd and epoll logic
+ * for testing.
+ */
+class IClock {
+public:
+    /**
+     * A handle to a timer or poll instance.
+     */
+    using Handle = int;
+
+    // like POSIX file descriptors, valid handles are non-negative.
+    static constexpr Handle INVALID_HANDLE = -1;  // invalid
+    static constexpr Handle PENDING_HANDLE = -2;  // operation still pending, retry later
+    static constexpr Handle INTR_HANDLE = -3;     // interrupt occurred, retry immediate
+
+    /**
+     * The type of clock to use for a timer.
+     */
+    enum ClockType {
+        BOOTTIME,        // time in nanos since device boot, will not wake from suspend.
+        BOOTTIME_ALARM,  // time in nanos since device boot, will wake from suspend.
+    };
+
+    virtual ~IClock() = default;
+
+    /**
+     * Creates a new timer.
+     * @param clockType The type of clock to use.
+     * @return A handle to the new timer, or INVALID_HANDLE on error.
+     */
+    virtual Handle createTimer(ClockType clockType) = 0;
+
+    /**
+     * Destroys a timer.
+     * @param handle The handle of the timer to destroy.
+     * @return OK on success, or a negative error code.
+     */
+    virtual status_t destroyTimer(Handle handle) = 0;
+
+    /**
+     * Returns true if the clock has been successfully initialized and is ready to be used.
+     */
+    virtual bool ready() const = 0;
+
+    /**
+     * Sets a one-shot timer.
+     * @param handle The handle of the timer to set.
+     * @param time The absolute time in nanoseconds for the timer to expire.
+     *             A time of 0 disables the timer.
+     * @return OK on success, or a negative error code.
+     */
+    virtual status_t setTimer(Handle handle, nsecs_t time) = 0;
+
+    /**
+     * Waits for a timer to expire.
+     * @param timeout The maximum time to wait in nanoseconds. A timeout of -1
+     *                  waits forever.
+     * @return The handle of the expired timer, PENDING_HANDLE on timeout,
+     *         INTR_HANDLE on system interrupt, or INVALID_HANDLE on error.
+     */
+    virtual Handle wait(nsecs_t timeout) = 0;
+
+    /**
+     * Creates a new LinuxClock instance.
+     */
+    static std::unique_ptr<IClock> createLinuxClock();
+};
 
 /**
  * A timed execution queue for scheduling functions to run at specific times.
@@ -47,6 +120,15 @@ public:
      * @param alarm set to true to allow a wake from suspend.
      */
     explicit TimerQueue(bool alarm = false);
+
+    /**
+     * TimerQueue ctor for testing.
+     *
+     * @param clock A clock interface for dependency injection.
+     * @param alarm set to true to allow a wake from suspend.
+     */
+    TimerQueue(std::unique_ptr<IClock> clock, bool alarm);
+
     ~TimerQueue();
 
     /**
@@ -55,8 +137,12 @@ public:
      * This handle is returned by `add()` and can be used with `remove()` to
      * cancel a pending event before it has executed.
      */
-    using handle_t = int64_t;
-    static constexpr handle_t INVALID_HANDLE = -1;
+    using EventId = int64_t;
+    static constexpr EventId INVALID_EVENT_ID = -1;
+
+    // remove later
+    using handle_t = EventId;
+    static constexpr auto INVALID_HANDLE = INVALID_EVENT_ID;
 
     /**
      * Schedules a function to be executed at a specific time.
@@ -67,11 +153,40 @@ public:
      *                      `CLOCK_BOOTTIME` monotonic clock, at which the
      *                      function should be executed.
      *
-     * @return A unique `handle_t` that can be used to cancel the event with
-     *         `remove()`. Returns `INVALID_HANDLE` if the provided function
+     * @return A unique `EventId` that can be used to cancel the event with
+     *         `remove()`. Returns `INVALID_EVENT_ID` if the provided function
      *         is null.
      */
-    handle_t add(std::function<void()> function, nsecs_t executionTime);
+    EventId add(std::function<void()> function, nsecs_t executionTime);
+
+    /**
+     * Schedules a function to be executed between softDeadline and hardDeadline.
+     *
+     * The softDeadline will not trigger if suspended.  The hardDeadline will
+     * trigger if suspended.
+     *
+     * The TimerQueue must be initialized with alarm true for the hardDeadline
+     * to work properly, otherwise softDeadline is only used.
+     *
+     * @param function The `std::function<void()>` to execute. The function
+     *                 should not be null.
+     * @param softDeadline The earliest absolute time in nanoseconds, based on the
+     *                     `CLOCK_BOOTTIME` monotonic clock, at which the
+     *                     function should be executed.
+     * @param hardDeadline The latest absolute time in nanoseconds, based on the
+     *                     `CLOCK_BOOTTIME` monotonic clock, at which the
+     *                     function should be executed.  This is ignored if
+     *                     mAlarm is false.
+     * @param priorityTime The time used to determine which task is scheduled
+     *                     if multiple tasks can be executed.  Defaults to the
+     *                     hardDeadline if unspecified (or negative).
+     *
+     * @return A unique `EventId` that can be used to cancel the event with
+     *         `remove()`. Returns `INVALID_EVENT_ID` if the provided function
+     *         is null.
+     */
+    EventId add(std::function<void()> function,
+        nsecs_t softDeadline, nsecs_t hardDeadline, nsecs_t priorityTime = -1);
 
     /**
      * Removes a pending function from the execution queue.
@@ -80,17 +195,17 @@ public:
      * will be removed and will not be called. If the event has already started
      * executing or has finished, this method does nothing.
      *
-     * @param handle The handle of the event to remove, as returned by `add()`.
+     * @param id The id of the event to remove, as returned by `add()`.
      *
      * @return `true` if the event was successfully found and removed before
      *         execution, `false` otherwise.
      */
-    bool remove(handle_t handle);
+    bool remove(EventId id);
 
     /**
      * Returns true if the timer was successfully initialized.
      */
-    bool ready() const { return mTimerFd >= 0; }
+    bool ready() const { return mClock->ready(); }
 
     /**
      * Returns true if the timer can wake from suspend.
@@ -98,26 +213,47 @@ public:
     bool alarm() const { return mAlarm; }
 
 private:
+    EventId getNextEventId_l() REQUIRES(mMutex);
     void threadLoop();
-    void armTimerForNextEvent_l() REQUIRES(mMutex);
 
     struct Event {
+        EventId id;
         std::function<void()> function;
-        nsecs_t executionTime;
+        nsecs_t priorityTime;
     };
 
+    // AlarmClock is not inherently thread-safe, but accessed with ThreadQueue mMutex held.
+    class AlarmClock {
+    public:
+        AlarmClock(IClock* clock, IClock::ClockType clockType, bool& running);
+        ~AlarmClock();
+
+        IClock::Handle getHandle() const { return mTimerHandle; }
+        void add(nsecs_t executionTime, const std::shared_ptr<Event>& event);
+        bool remove(EventId id);
+        void armTimerForNextEvent();
+        void collectEvents(nsecs_t now, std::set<std::shared_ptr<Event>>& events);
+        void removeEvents(const std::set<std::shared_ptr<Event>>& events);
+
+    private:
+        IClock* const mClock;
+        const IClock::Handle mTimerHandle;
+        bool& mRunning;  // from the outer object always read safely.
+
+        // Main storage for event data, keyed by id.
+        std::map<EventId, std::pair<std::shared_ptr<Event>, nsecs_t>> mEvents;
+
+        // Index to keep events sorted by time for efficient lookup of the next event.
+        std::multimap <nsecs_t, EventId> mTimeIndex;
+    };
+
+    std::unique_ptr<IClock> mClock;
     const bool mAlarm;  // if true, in ALARM mode and will wake from suspend.
-    const int mTimerFd;
     std::mutex mMutex;
     std::thread mThread;  // effectively const
-    bool mRunning GUARDED_BY(mMutex) = true;
-    handle_t mNextHandle GUARDED_BY(mMutex) = 1;
-
-    // Main storage for event data, keyed by handle.
-    std::map<handle_t, Event> mEvents GUARDED_BY(mMutex);
-
-    // Index to keep events sorted by time for efficient lookup of the next event.
-    std::multimap<nsecs_t, handle_t> mTimeIndex GUARDED_BY(mMutex);
+    bool mRunning GUARDED_BY(mMutex) = false;
+    EventId mNextEventId GUARDED_BY(mMutex) = 1;
+    std::vector<AlarmClock> mAlarmClocks GUARDED_BY(mMutex);
 };
 
 } // namespace android::audio_utils
